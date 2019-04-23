5649a701acd7a7da5e232579929de6cd53b3c485
Mateen Deshmukh Mohammed Taher <mdeshmukh@AljabrGroup.loc>
2019-04-23 14:17:30 +0300

    Add project files.

---
+++ MyFirstCore/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstCore.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
